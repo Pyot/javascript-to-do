e2d1e1b77e2ca99c90c8896a63ddc6f5f62346ef
Piotr Ludwig <piotr.a.ludwig@gmail.com>
2017-01-24 12:28:27 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,111 @@
+var removeSVG = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect class="noFill" width="22" height="22"/><g><g><path class="fill" d="M16.1,3.6h-1.9V3.3c0-1.3-1-2.3-2.3-2.3h-1.7C8.9,1,7.8,2,7.8,3.3v0.2H5.9c-1.3,0-2.3,1-2.3,2.3v1.3c0,0.5,0.4,0.9,0.9,1v10.5c0,1.3,1,2.3,2.3,2.3h8.5c1.3,0,2.3-1,2.3-2.3V8.2c0.5-0.1,0.9-0.5,0.9-1V5.9C18.4,4.6,17.4,3.6,16.1,3.6z M9.1,3.3c0-0.6,0.5-1.1,1.1-1.1h1.7c0.6,0,1.1,0.5,1.1,1.1v0.2H9.1V3.3z M16.3,18.7c0,0.6-0.5,1.1-1.1,1.1H6.7c-0.6,0-1.1-0.5-1.1-1.1V8.2h10.6V18.7z M17.2,7H4.8V5.9c0-0.6,0.5-1.1,1.1-1.1h10.2c0.6,0,1.1,0.5,1.1,1.1V7z"/></g><g><g><path class="fill" d="M11,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6s0.6,0.3,0.6,0.6v6.8C11.6,17.7,11.4,18,11,18z"/></g><g><path class="fill" d="M8,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C8.7,17.7,8.4,18,8,18z"/></g><g><path class="fill" d="M14,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C14.6,17.7,14.3,18,14,18z"/></g></g></g></svg>';
+var completeSVG = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect y="0" class="noFill" width="22" height="22"/><g><path class="fill" d="M9.7,14.4L9.7,14.4c-0.2,0-0.4-0.1-0.5-0.2l-2.7-2.7c-0.3-0.3-0.3-0.8,0-1.1s0.8-0.3,1.1,0l2.1,2.1l4.8-4.8c0.3-0.3,0.8-0.3,1.1,0s0.3,0.8,0,1.1l-5.3,5.3C10.1,14.3,9.9,14.4,9.7,14.4z"/></g></svg>';
+
+var upSVG = '<?xml version="1.0" encoding="utf-8"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="64px" height="64px" viewBox="0 0 64 64" enable-background="new 0 0 64 64" xml:space="preserve"><g id="eye"><path fill="#1D1D1B" d="M3.352,48.296l28.56-28.328l28.58,28.347c0.397,0.394,0.917,0.59,1.436,0.59c0.52,0,1.04-0.196,1.436-0.59c0.793-0.787,0.793-2.062,0-2.849l-29.98-29.735c-0.2-0.2-0.494-0.375-0.757-0.475c-0.75-0.282-1.597-0.107-2.166,0.456L0.479,45.447c-0.793,0.787-0.793,2.062,0,2.849C1.273,49.082,2.558,49.082,3.352,48.296z"/></g><g id="comment"></g><g id="map-marker"></g><g id="earth"></g><g id="heart"></g><g id="twitter"></g><g id="bucket"></g><g id="paperclip"></g><g id="undo"></g><g id="redo"></g><g id="chevron-left"></g><g id="chevron-right"></g><g id="cross"></g><g id="search"></g><g id="hamburger"></g></svg>';
+var downSVG = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="64px" height="64px" viewBox="0 0 64 64" enable-background="new 0 0 64 64" xml:space="preserve"><g id="eye"><path fill="#1D1D1B" d="M60.533,15.733L31.973,44.061L3.394,15.713c-0.397-0.394-0.917-0.59-1.437-0.59s-1.039,0.196-1.436,0.59c-0.793,0.787-0.793,2.062,0,2.849l29.98,29.735c0.2,0.2,0.494,0.375,0.757,0.476c0.75,0.282,1.597,0.107,2.166-0.456l29.981-29.735c0.793-0.787,0.793-2.062,0-2.849C62.611,14.947,61.327,14.947,60.533,15.733z"/></g><g id="comment"></g><g id="map-marker"></g><g id="earth"></g><g id="heart"></g><g id="twitter"></g><g id="bucket"></g><g id="paperclip"></g><g id="undo"></g><g id="redo"></g><g id="chevron-left"></g><g id="chevron-right"></g><g id="cross"></g><g id="search"></g><g id="hamburger"></g></svg>';
+  
+//User clicked on add button
+document.getElementById('add').addEventListener('click', function() {
+    var value = document.getElementById('item').value;
+    
+    if (value) {
+        
+        addItemToDo(value);
+        document.getElementById('item').value = '';
+    };
+    
+    
+});
+    
+function removeItem() {
+    var item = this.parentNode.parentNode;
+    var parent = item.parentNode;
+    
+    parent.removeChild(item);
+    
+}
+
+function moveUpItem() {
+    var item = this.parentNode.parentNode;
+    var parent = item.parentNode;
+    if (item === parent.firstChild){console.log("can not do it is firs item on list")
+    } else {
+      item.parentNode.insertBefore(item, item.previousSibling);  
+    }
+    console.log(item);
+}
+
+function moveDownItem1() {
+    var item = this.parentNode.parentNode;
+    var parent = item.parentNode;
+    $( item ).insertAfter( $( item.nextSibling) );
+
+    
+}
+
+
+function completedItem() {
+var item = this.parentNode.parentNode;
+var parent = item.parentNode;
+    var id = parent.id;
+
+    
+
+
+//Check if the item shoudl be added to completed to re-added todo
+var target = (id === 'todo') ? document.getElementById('completed'):document.getElementById('todo');
+
+
+
+    parent.removeChild(item);
+    target.insertBefore(item, target.childNodes[0]);
+
+}
+
+function addItemToDo(text) {
+    var list = document.getElementById('todo');
+    
+    var item = document.createElement('li');
+    item.innerText = text.substring(0,35);
+    
+    var buttons = document.createElement('div');
+    buttons.classList.add('buttons');
+    
+    var moveUp = document.createElement('button');
+    moveUp.classList.add('updown');
+    moveUp.innerHTML = upSVG;
+    
+    //Move up the todo position
+    moveUp.addEventListener('click', moveUpItem);
+    
+    var moveDown = document.createElement('button');
+    moveDown.classList.add('updown');
+    moveDown.innerHTML = downSVG;
+    
+    moveDown.addEventListener('click', moveDownItem1);
+    
+    var remove = document.createElement('button');
+    remove.classList.add('remove');
+    remove.innerHTML = removeSVG;
+    
+    //Add clikc event for removing item
+    remove.addEventListener('click', removeItem);
+    
+    var complete = document.createElement('button');
+    complete.classList.add('complete');
+    complete.innerHTML = completeSVG;
+    
+    //Add click event for moving to completed list
+    
+    complete.addEventListener('click', completedItem);
+    
+    buttons.appendChild(moveUp);
+    buttons.appendChild(moveDown);
+    buttons.appendChild(remove);
+    buttons.appendChild(complete);
+    
+    item.appendChild(buttons);
+    
+    list.insertBefore(item, list.childNodes[0]);
+    
+}
